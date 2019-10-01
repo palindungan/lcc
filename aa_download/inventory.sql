e040671f5c8bc31f7ac08be2930750d93f7bf84b
zalramli <github.little@gmail.com>
2019-10-01 16:13:48 +0700

    updatdatabasee

--- aa_download/inventory.sql
+++ aa_download/inventory.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Waktu pembuatan: 30 Sep 2019 pada 08.41
+-- Waktu pembuatan: 01 Okt 2019 pada 11.13
 -- Versi server: 10.4.6-MariaDB
 -- Versi PHP: 7.3.9
 
@@ -25,6 +25,20 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Stand-in struktur untuk tampilan `barang_kasir`
+-- (Lihat di bawah untuk tampilan aktual)
+--
+CREATE TABLE `barang_kasir` (
+`id_stok_b` int(7)
+,`kode_unik` varchar(30)
+,`nama` varchar(50)
+,`tanggal` timestamp
+,`barcode` varchar(30)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `barang_terdaftar`
 --
 
@@ -33,6 +47,13 @@ CREATE TABLE `barang_terdaftar` (
   `nama` varchar(50) NOT NULL,
   `barcode` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `barang_terdaftar`
+--
+
+INSERT INTO `barang_terdaftar` (`kode`, `nama`, `barcode`) VALUES
+('B00001', 'Asus X453 MA', 'kosong');
 
 -- --------------------------------------------------------
 
@@ -45,6 +66,16 @@ CREATE TABLE `customer` (
   `nama` varchar(50) NOT NULL,
   `no_hp` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `customer`
+--
+
+INSERT INTO `customer` (`id_customer`, `nama`, `no_hp`) VALUES
+('C20190930001', 'Ari', '0812963989'),
+('C20191001001', 'Ali Nadzim', '089685746747'),
+('C20191001002', 'Arixx', '0821112213123'),
+('C20191001003', 'afri', '089123771235');
 
 -- --------------------------------------------------------
 
@@ -60,6 +91,16 @@ CREATE TABLE `detail_penjualan` (
   `qty` int(3) NOT NULL,
   `total_hrg` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `detail_penjualan`
+--
+
+INSERT INTO `detail_penjualan` (`id_detail_pj`, `id_penjualan`, `id_stok_b`, `harga_jual`, `qty`, `total_hrg`) VALUES
+(1, 'P20190930002', '1', 3000000, 3, 9000000),
+(2, 'P20191001001', '1', 3000000, 2, 6000000),
+(3, 'P20191001002', '1', 1000000, 2, 2000000),
+(4, 'P20191001003', '1', 3000000, 1, 3000000);
 
 -- --------------------------------------------------------
 
@@ -87,6 +128,13 @@ CREATE TABLE `pemasokan` (
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `total` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `pemasokan`
+--
+
+INSERT INTO `pemasokan` (`id_pemasokan`, `id_user`, `id_distributor`, `tanggal`, `total`) VALUES
+('M2019093001', 'U01', 'D01', '2019-09-05 07:17:28', 3500000);
 
 -- --------------------------------------------------------
 
@@ -118,6 +166,16 @@ CREATE TABLE `penjualan` (
   `kembalian` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_user`, `id_customer`, `tanggal`, `total`, `bayar`, `kembalian`) VALUES
+('P20190930002', 'U01', 'C20190930001', '2019-09-30 02:53:52', 9000000, 10000000, 1000000),
+('P20191001001', 'U01', 'C20191001001', '2019-10-01 01:05:16', 9000000, 10000000, 1000000),
+('P20191001002', 'U01', 'C20191001002', '2019-10-01 01:21:38', 2000000, 2000000, 0),
+('P20191001003', 'U01', 'C20191001003', '2019-10-01 01:23:52', 3000000, 4000000, 1000000);
+
 -- --------------------------------------------------------
 
 --
@@ -134,6 +192,13 @@ CREATE TABLE `stok_barang` (
   `kode_unik` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data untuk tabel `stok_barang`
+--
+
+INSERT INTO `stok_barang` (`id_stok_b`, `id_pemasokan`, `kode`, `qty`, `hrg_distributor`, `total_hrg`, `kode_unik`) VALUES
+(1, 'M2019093001', 'B00001', 1, 3500000, 3500000, 'LXRH10C0101');
+
 -- --------------------------------------------------------
 
 --
@@ -142,7 +207,7 @@ CREATE TABLE `stok_barang` (
 
 CREATE TABLE `toko` (
   `id_toko` char(2) NOT NULL,
-  `nama` varchar(50) NOT NULL,
+  `nama_toko` varchar(50) NOT NULL,
   `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -154,12 +219,21 @@ CREATE TABLE `toko` (
 
 CREATE TABLE `user` (
   `id_user` char(3) NOT NULL,
-  `nama` varchar(50) NOT NULL,
+  `nama_user` varchar(50) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` char(60) NOT NULL,
   `jenis_akses` enum('Manager','Kasir') NOT NULL,
   `id_toko` char(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur untuk view `barang_kasir`
+--
+DROP TABLE IF EXISTS `barang_kasir`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `barang_kasir`  AS  select `stok_barang`.`id_stok_b` AS `id_stok_b`,`stok_barang`.`kode_unik` AS `kode_unik`,`barang_terdaftar`.`nama` AS `nama`,`pemasokan`.`tanggal` AS `tanggal`,`barang_terdaftar`.`barcode` AS `barcode` from ((`stok_barang` join `pemasokan` on(`stok_barang`.`id_pemasokan` = `pemasokan`.`id_pemasokan`)) join `barang_terdaftar` on(`stok_barang`.`kode` = `barang_terdaftar`.`kode`)) ;
 
 --
 -- Indexes for dumped tables
@@ -233,13 +307,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `detail_penjualan`
 --
 ALTER TABLE `detail_penjualan`
-  MODIFY `id_detail_pj` int(7) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail_pj` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT untuk tabel `stok_barang`
 --
 ALTER TABLE `stok_barang`
-  MODIFY `id_stok_b` int(7) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_stok_b` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
