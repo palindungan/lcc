58a9ed871615bc01cfaaafdc0cc20834d61a13a3
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-02 09:30:28 +0700

    Update tampil.php

--- application/views/view_1/konten/manager/pemasokan/tampil.php
+++ application/views/view_1/konten/manager/pemasokan/tampil.php
@@ -30,62 +30,106 @@
 <!-- Breadcomb area End-->
 
 <!-- Form Element area Start-->
-<div class="form-element-list">
-    <form id="transaksi_form" method="post">
 
-        <div class="basic-tb-hd">
-            <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                    <div class="form-group">
-                        <div class="bootstrap-select fm-cmp-mg">
-                            <select id="cari_kode_barang" class="selectpicker" data-live-search="true">
-
-                                <option value="">Cari Barang</option>
-
-                                <option>Nama Barang</option>
-
-
-                            </select>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                    <div class="form-group">
-                        <a id="add_more" name="add_more" class="btn btn-primary">+</a>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                    <div class="form-group">
-                        <button onclick="return confirm('Yakin ingin Mengirim data Permintaan ?')" id="action" type="submit" name="simpan" class="btn btn-primary mr-2">Simpan Pemesanan</button>
-                    </div>
-                </div>
-                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
-                    <div class="form-group">
-                        <a onclick="window.history.back();" class="btn btn-danger">Kembali</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
-                <label>No</label>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <label>Kode Barang</label>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <label>Nama Barang</label>
-            </div>
-            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                <label>Jumlah Barang</label>
-            </div>
-        </div>
-
-        <div id="span_product_details">
-            <!-- disini isi detail -->
-        </div>
-
-    </form>
-</div>
 <!-- Form Element area End-->
+
+<div class="form-element-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="form-element-list">
+                    <form id="transaksi_form" method="post">
+
+                        <div class="basic-tb-hd">
+                            <div class="row">
+                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                    <div class="form-group">
+                                        <div class="bootstrap-select fm-cmp-mg">
+                                            <select class="selectpicker" data-live-search="true">
+                                                <option value="">Pilih Distributor</option>
+                                                <option>Distributor 1</option>
+                                                <option>Distributor 2</option>
+                                                <option>Distributor 3</option>
+                                                <option>Distributor 4</option>
+                                                <option>Distributor 5</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                    <div class="form-group nk-datapk-ctm form-elet-mg" id="data_1">
+                                        <div class="input-group date nk-int-st">
+                                            <span class="input-group-addon"></span>
+                                            <input type="text" class="form-control" value="" placeholder="Tanggal">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Total" readonly>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                    <div class="form-group">
+                                        <button class="form-control btn btn-primary"><i class="notika-icon notika-search"></i> Cari Barang</button>
+                                    </div>
+                                </div>
+                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                    <div class="form-group">
+                                        <button class="form-control btn btn-primary">+ Tambah Baris</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                <label>Kode</label>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                <label>Nama</label>
+                            </div>
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                <label>Qty</label>
+                            </div>
+                            <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                <label>Harga</label>
+                            </div>
+                            <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                <label>Hapus</label>
+                            </div>
+                        </div>
+
+                        <div id="span_product_details">
+                            <!-- disini isi detail -->
+
+                            <div id="row" class="row">
+                                <br />
+                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                    <input type="text" class="form-control" placeholder="Kode/Barcode" value="">
+                                </div>
+                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                    <input type="text" class="form-control" placeholder="Nama Barang" value="">
+                                </div>
+                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12">
+                                    <input type="number" class="form-control" placeholder="qty" value="">
+                                </div>
+                                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+                                    <input type="number" class="form-control" placeholder="Harga" value="">
+                                </div>
+                                <div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">
+                                    <button id="" name="remove" class="remove btn btn-danger"><i class="notika-icon notika-trash"></i></button>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
