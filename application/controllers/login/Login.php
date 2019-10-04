d722e792d72427e17142f1831e5274748cb4ad18
zalramli <github.little@gmail.com>
2019-10-04 21:20:51 +0700

    login

---
+++ application/controllers/login/Login.php
@@ -0,0 +1,56 @@
+<?php
+class Login extends CI_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('login/M_login');
+    }
+    public function index()
+    {
+        $this->load->view('view_1/login/login');
+    }
+    public function aksi_login(){
+        
+        $cek = $this->M_login->cek_login();
+        if($cek->num_rows() > 0)
+            {
+                foreach($cek->result() as $row){
+                $id_user = $row->id_user;
+                $username = $row->username;
+                $nama = $row->nama_user;
+                $akses = $row->jenis_akses;
+                $password = $row->password;
+                $id_toko = $row->id_toko;
+                $nama_toko = $row->nama_toko;
+                }
+                $data_session = array(
+                    'id_user' => $id_user,
+                    'username' => $username,
+                    'nama' => $nama,
+                    'akses' => $akses,
+                    'password' => $password,
+                    'id_toko' => $id_toko,
+                    'nama_toko' => $nama_toko
+                );
+                $this->session->set_userdata($data_session);
+                if($row->jenis_akses == 'Manager')
+                {
+                    redirect(base_url("dashboard_manager"));
+                }
+                else if($row->jenis_akses == 'Kasir')
+                {
+                    redirect(base_url("kasir"));
+                }
+            }
+            else
+            {
+                echo "login gagal";
+            }
+    }
+    public function logout()
+    {
+        $this->session->sess_destroy();
+        redirect(base_url('login/login'));
+    }
+}
---
+++ application/models/login/M_login.php
@@ -0,0 +1,16 @@
+<?php 
+ 
+class M_login extends CI_Model
+{
+	public function cek_login(){
+        $where = array(
+        'username' => $this->input->post('username'),
+        'password' => $this->input->post('password')
+        );
+        $this->db->select('*'); // Select field
+        $this->db->from('user'); // from Table1
+        $this->db->join('toko','user.id_toko = toko.id_toko','INNER');
+        $this->db->where($where);
+	    return $this->db->get();
+	}
+}
