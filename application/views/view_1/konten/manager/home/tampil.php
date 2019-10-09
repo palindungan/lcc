88bad0ed55a4f23880ece5f979ebbdd4984432e4
zalramli <github.little@gmail.com>
2019-10-09 17:33:06 +0700

    --

--- application/views/view_1/konten/manager/home/tampil.php
+++ application/views/view_1/konten/manager/home/tampil.php
@@ -96,8 +96,7 @@
 	<div class="row">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			<div id="chart_div" style="height: 500px;"></div>
-		</div </div> </div> <div class="container">
-
+		</div>
 	</div>
 </div>
 
