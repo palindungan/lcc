29f70c5188ccaed0eb5cdd2438907534a02c0a2d
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-11 13:43:09 +0700

    formating code
    formating code

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -10,43 +10,38 @@ class Login extends CI_Controller
     {
         $this->load->view('view_1/login/login');
     }
-    public function aksi_login(){
+    public function aksi_login()
+    {
         $userpass = $this->input->post('password');
         $cek = $this->M_login->cek_login();
         $cek_pimpinan = $this->M_login->cek_pimpinan();
-        if($cek_pimpinan->num_rows() > 0)
-        {
-            foreach($cek_pimpinan->result() as $row_pimpinan){
-            $id_user_pimpinan = $row_pimpinan->id_user_p;
-            $username_pimpinan = $row_pimpinan->username;
-            $nama_pimpinan = $row_pimpinan->nama;
-            $akses_pimpinan = $row_pimpinan->jenis_akses;
-            $password_pimpinan = $row_pimpinan->password;
-        }
+        if ($cek_pimpinan->num_rows() > 0) {
+            foreach ($cek_pimpinan->result() as $row_pimpinan) {
+                $id_user_pimpinan = $row_pimpinan->id_user_p;
+                $username_pimpinan = $row_pimpinan->username;
+                $nama_pimpinan = $row_pimpinan->nama;
+                $akses_pimpinan = $row_pimpinan->jenis_akses;
+                $password_pimpinan = $row_pimpinan->password;
+            }
             if (password_verify($userpass, $password_pimpinan)) {
-            $data_session = array(
-            'id_user' => $id_user_pimpinan,
-            'username' => $username_pimpinan,
-            'nama' => $nama_pimpinan,
-            'akses' => $akses_pimpinan,
-            'password' => $password_pimpinan,
-            'nama_toko' => 'Inventory Management'
-            );
-        $this->session->set_userdata($data_session);
-        if($row_pimpinan->jenis_akses == 'Pimpinan')
-        {
-        redirect(base_url("dashboard_pimpinan"));
+                $data_session = array(
+                    'id_user' => $id_user_pimpinan,
+                    'username' => $username_pimpinan,
+                    'nama' => $nama_pimpinan,
+                    'akses' => $akses_pimpinan,
+                    'password' => $password_pimpinan,
+                    'nama_toko' => 'Inventory Management'
+                );
+                $this->session->set_userdata($data_session);
+                if ($row_pimpinan->jenis_akses == 'Pimpinan') {
+                    redirect(base_url("dashboard_pimpinan"));
+                }
+            }
+        } else {
+            echo "login gagal";
         }
-        
-        }
-        }
-        else
-        {
-        echo "login gagal";
-        }
-        if($cek->num_rows() > 0)
-            {
-                foreach($cek->result() as $row){
+        if ($cek->num_rows() > 0) {
+            foreach ($cek->result() as $row) {
                 $id_user = $row->id_user;
                 $username = $row->username;
                 $nama = $row->nama_user;
@@ -54,32 +49,27 @@ class Login extends CI_Controller
                 $password = $row->password;
                 $id_toko = $row->id_toko;
                 $nama_toko = $row->nama_toko;
-                }
-                if (password_verify($userpass, $password)) {
+            }
+            if (password_verify($userpass, $password)) {
                 $data_session = array(
-                'id_user' => $id_user,
-                'username' => $username,
-                'nama' => $nama,
-                'akses' => $akses,
-                'password' => $password,
-                'id_toko' => $id_toko,
-                'nama_toko' => $nama_toko
+                    'id_user' => $id_user,
+                    'username' => $username,
+                    'nama' => $nama,
+                    'akses' => $akses,
+                    'password' => $password,
+                    'id_toko' => $id_toko,
+                    'nama_toko' => $nama_toko
                 );
                 $this->session->set_userdata($data_session);
-                if($row->jenis_akses == 'Manager')
-                {
-                redirect(base_url("dashboard_manager"));
-                }
-                else if($row->jenis_akses == 'Kasir')
-                {
-                redirect(base_url("kasir"));
-                }
+                if ($row->jenis_akses == 'Manager') {
+                    redirect(base_url("dashboard_manager"));
+                } else if ($row->jenis_akses == 'Kasir') {
+                    redirect(base_url("kasir"));
                 }
             }
-            else
-            {
-                echo "login gagal";
-            }
+        } else {
+            echo "login gagal";
+        }
     }
     public function logout()
     {
