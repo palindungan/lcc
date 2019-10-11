86850a15d6bb37bd77739f127e1db75668493986
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-11 14:27:14 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '192.168.43.112',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
 	'database' => 'inventory',
