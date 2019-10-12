455b526f364b2e9520e4c0659dd73bf950968fea
zalramli <github.little@gmail.com>
2019-10-12 09:00:23 +0700

    --

--- application/views/view_1/konten/manager/pemasokan/daftar_pemasokan.php
+++ application/views/view_1/konten/manager/pemasokan/daftar_pemasokan.php
@@ -1,88 +1,92 @@
 <!-- Breadcomb area Start-->
 <div class="breadcomb-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="breadcomb-list">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <div class="breadcomb-wp">
-                                <div class="breadcomb-icon">
-                                    <i class="notika-icon notika-form"></i>
-                                </div>
-                                <div class="breadcomb-ctn">
-                                    <h2>DAFTAR PEMASOKAN</h2>
-                                    <p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
-                            <div class="breadcomb-report">
-                                <button data-toggle="tooltip" data-placement="left" title="Download Report" class="btn"><i class="notika-icon notika-sent"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="breadcomb-list">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+							<div class="breadcomb-wp">
+								<div class="breadcomb-icon">
+									<i class="notika-icon notika-form"></i>
+								</div>
+								<div class="breadcomb-ctn">
+									<h2>DAFTAR PEMASOKAN</h2>
+									<p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
+							<div class="breadcomb-report">
+								<button data-toggle="tooltip" data-placement="left" title="Download Report"
+									class="btn"><i class="notika-icon notika-sent"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <!-- Breadcomb area End-->
 
 <!-- Form Element area Start-->
 <div class="data-table-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="data-table-list">
-                    <div class="basic-tb-hd">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="data-table-list">
+					<div class="basic-tb-hd">
 
-                    </div>
-                    <div class="table-responsive">
-                        <table id="data-table-basic" class="table table-striped">
-                            <thead>
-                                <tr>
-                                    <th>Kode</th>
-                                    <th>Distributor</th>
-                                    <th>Tanggal</th>
-                                    <th>Total</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
+					</div>
+					<div class="table-responsive">
+						<table id="data-table-basic" class="table table-striped">
+							<thead>
+								<tr>
+									<th>Kode</th>
+									<th>Distributor</th>
+									<th>Tanggal</th>
+									<th>Waktu</th>
+									<th>Total</th>
+									<th>Aksi</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
                                 foreach ($pemasokan_list as $row) {
                                     ?>
-                                    <tr>
-                                        <td><?= $row->id_pemasokan; ?></td>
-                                        <td><?= $row->nama; ?></td>
-                                        <td><?= $row->tanggal; ?></td>
-                                        <td><?= $row->nama_user; ?></td>
-                                        <td>
-                                            <div class="table-actions">
-                                                <a class="btn btn-primary fa fa-search" href="<?php echo base_url("pemasokan/detail_pemasokan?id_pemasokan=" . $row->id_pemasokan) ?>"></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                <?php
+								<tr>
+									<td><?= $row->id_pemasokan; ?></td>
+									<td><?= $row->nama; ?></td>
+									<td><?= date('d F Y', strtotime($row->tanggal)) ?></td>
+									<td><?= date('h:i:s', strtotime($row->tanggal)) ?></td>
+									<td><?= rupiah($row->total) ?></td>
+									<td>
+										<div class="table-actions">
+											<a class="btn btn-primary fa fa-search"
+												href="<?php echo base_url("pemasokan/detail_pemasokan?id_pemasokan=" . $row->id_pemasokan) ?>"></a>
+										</div>
+									</td>
+								</tr>
+								<?php
                                 }
                                 ?>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th>Kode</th>
-                                    <th>Distributor</th>
-                                    <th>Tanggal</th>
-                                    <th>Total</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </tfoot>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+							</tbody>
+							<tfoot>
+								<tr>
+									<th>Kode</th>
+									<th>Distributor</th>
+									<th>Tanggal</th>
+									<th>Total</th>
+									<th>Aksi</th>
+								</tr>
+							</tfoot>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <!-- Form Element area End-->
--- application/views/view_1/konten/manager/pemasokan/detail_pemasokan.php
+++ application/views/view_1/konten/manager/pemasokan/detail_pemasokan.php
@@ -1,31 +1,32 @@
 <!-- Breadcomb area Start-->
 <div class="breadcomb-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="breadcomb-list">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                            <div class="breadcomb-wp">
-                                <div class="breadcomb-icon">
-                                    <i class="notika-icon notika-form"></i>
-                                </div>
-                                <div class="breadcomb-ctn">
-                                    <h2>DETAIL PEMASOKAN</h2>
-                                    <p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
-                            <div class="breadcomb-report">
-                                <button data-toggle="tooltip" data-placement="left" title="Download Report" class="btn"><i class="notika-icon notika-sent"></i></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="breadcomb-list">
+					<div class="row">
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+							<div class="breadcomb-wp">
+								<div class="breadcomb-icon">
+									<i class="notika-icon notika-form"></i>
+								</div>
+								<div class="breadcomb-ctn">
+									<h2>DETAIL PEMASOKAN</h2>
+									<p>Welcome to Notika <span class="bread-ntd">Admin Template</span></p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 col-xs-3">
+							<div class="breadcomb-report">
+								<button data-toggle="tooltip" data-placement="left" title="Download Report"
+									class="btn"><i class="notika-icon notika-sent"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <!-- Breadcomb area End-->
 
@@ -41,73 +42,73 @@ foreach ($pemasokan_list as $row) {
 
 <!-- Form Element area Start-->
 <div class="data-table-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="data-table-list">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="data-table-list">
 
-                    <div style="margin-bottom: 20px;">
-                        <a onclick="window.history.back();" class="btn btn-primary btn-lg">Kembali</a>
-                    </div>
-                    <table class="table table-borderless" width="100">
-                        <tr>
-                            <th width="11%">Distributor</th>
-                            <th width="1%">:</th>
-                            <th><?= $nama_distributor; ?></th>
-                            <th width="11%">Manager</th>
-                            <th width="1%">:</th>
-                            <th><?= $nama_manager; ?></th>
-                        </tr>
-                        <tr>
-                            <th>Tanggal</th>
-                            <th>:</th>
-                            <th><?= $tanggal; ?></th>
-                            <th>Kode</th>
-                            <th>:</th>
-                            <th><?= $id_pemasokan; ?></th>
-                        </tr>
-                    </table>
-                    <table class="table table-sm table-borderless" width="100%">
-                        <thead>
-                            <tr>
-                                <th width="7%" scope="col">NO</th>
-                                <th width="25%" scope="col">NAMA BARANG</th>
-                                <th width="8%" scope="col">QTY</th>
-                                <th width="15%" scope="col">HARGA BARANG</th>
-                                <th width="15%" style="text-align:center" scope="col">TOTAL HARGA</th>
-                                <th width="15%">BARCODE</th>
-                                <th width="15%">KODE UNIK</th>
-                            </tr>
+					<div style="margin-bottom: 20px;">
+						<a onclick="window.history.back();" class="btn btn-primary btn-lg">Kembali</a>
+					</div>
+					<table class="table table-borderless" width="100">
+						<tr>
+							<th width="11%">Distributor</th>
+							<th width="1%">:</th>
+							<th><?= $nama_distributor; ?></th>
+							<th width="11%">Manager</th>
+							<th width="1%">:</th>
+							<th><?= $nama_manager; ?></th>
+						</tr>
+						<tr>
+							<th>Tanggal</th>
+							<th>:</th>
+							<th><?= $tanggal; ?></th>
+							<th>Kode</th>
+							<th>:</th>
+							<th><?= $id_pemasokan; ?></th>
+						</tr>
+					</table>
+					<table class="table table-sm table-borderless" width="100%">
+						<thead>
+							<tr>
+								<th width="7%" scope="col">NO</th>
+								<th width="25%" scope="col">NAMA BARANG</th>
+								<th width="8%" scope="col">QTY</th>
+								<th width="15%" scope="col">HARGA BARANG</th>
+								<th width="15%" style="text-align:center" scope="col">TOTAL HARGA</th>
+								<th width="15%">BARCODE</th>
+								<th width="15%">KODE UNIK</th>
+							</tr>
 
-                        </thead>
-                        <tbody>
-                            <?php
+						</thead>
+						<tbody>
+							<?php
                             foreach ($pemasokan_list_detail as $row) {
                                 ?>
-                                <tr>
-                                    <td width="7%" scope="row"><?= $row->id_pemasokan; ?></td>
-                                    <td width="25%"><?= $row->nama; ?></td>
-                                    <td width="8%"><?= ($row->total_hrg / $row->hrg_distributor); ?></td>
-                                    <td width="15%" style="text-align:right"><?= $row->hrg_distributor ?></td>
-                                    <td width="15%" style="text-align:right"><?= $row->total_hrg ?></td>
-                                    <td width="15%"><?= $row->barcode; ?></td>
-                                    <td width="15%"><?= $row->kode_unik; ?></td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                    <table class="table table-sm table-borderless">
-                        <tr>
-                            <th width="7%"></th>
-                            <th width="59%"></th>
-                            <th style="text-align:right" width="22%">Total</th>
-                            <th style="text-align:right"><?= $total; ?></th>
-                        </tr>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
+							<tr>
+								<td width="7%" scope="row"><?= $row->id_pemasokan; ?></td>
+								<td width="25%"><?= $row->nama; ?></td>
+								<td width="8%"><?= ($row->total_hrg / $row->hrg_distributor); ?></td>
+								<td width="15%" style="text-align:right"><?= rupiah($row->hrg_distributor) ?></td>
+								<td width="15%" style="text-align:right"><?= rupiah($row->total_hrg) ?></td>
+								<td width="15%"><?= $row->barcode; ?></td>
+								<td width="15%"><?= $row->kode_unik; ?></td>
+							</tr>
+							<?php } ?>
+						</tbody>
+					</table>
+					<table class="table table-sm table-borderless">
+						<tr>
+							<th width="7%"></th>
+							<th width="59%"></th>
+							<th style="text-align:right" width="22%">Total</th>
+							<th style="text-align:right"><?= rupiah($total) ?></th>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 
 <!-- Form Element area End-->
