97d14d9b22dc78ff00c0fa069c2262e0aeb5c0f3
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-19 05:53:17 +0700

    Update M_barcode.php

--- application/models/manager/M_barcode.php
+++ application/models/manager/M_barcode.php
@@ -5,7 +5,7 @@ class M_barcode extends CI_Model
 	function barang_toko_barcode()
 	{
 		$id_toko = $this->session->userdata('id_toko');
-		return $this->db->order_by('id_stok_b', 'DESC')->get_where('barang_kasir', array(
+		return $this->db->order_by('id_stok_b', 'DESC')->get_where('barang_toko_barcode', array(
 			'id_toko' => $id_toko
 		));
 	}
