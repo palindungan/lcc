f08512ecb650f2c0fe04df98ef99c0f3aac7448e
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-18 23:05:15 +0700

    Update M_home.php

--- application/models/kasir/M_home.php
+++ application/models/kasir/M_home.php
@@ -2,22 +2,23 @@
 class M_home extends CI_Model
 {
 
-	function barang_kasir()
-	{
-		$id_toko = $this->session->userdata('id_toko');
-		return $this->db->get_where('barang_kasir', array('id_toko' => $id_toko, 'qty >' => 0))->result();
-	}
-	function input_data($data, $table)
-	{
-		$this->db->insert($table, $data);
-	}
-	function id_customer()
-	{
-		$field = "id_customer";
+    function barang_kasir()
+    {
+        $id_toko = $this->session->userdata('id_toko');
+        return $this->db->get_where('barang_kasir', array('id_toko' => $id_toko, 'qty >' => 0))->result();
+    }
+    function input_data($data, $table)
+    {
+        $this->db->insert($table, $data);
+    }
+    function id_customer()
+    {
+        $field = "id_customer";
         $tabel = "customer";
         $digit = "3";
+        $ymd = date('ymd');
 
-        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel LIMIT 1");
+        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel WHERE SUBSTR($field, 2, 6) = $ymd LIMIT 1");
         $kd = "";
         if ($q->num_rows() > 0) {
             foreach ($q->result() as $k) {
@@ -28,15 +29,16 @@ class M_home extends CI_Model
             $kd = "001";
         }
         date_default_timezone_set('Asia/Jakarta');
-        return 'C' . date('dmy') . $kd;
-	}
-	function id_penjualan()
-	{
-		$field = "id_penjualan";
+        return 'C' . date('ymd') . $kd;
+    }
+    function id_penjualan()
+    {
+        $field = "id_penjualan";
         $tabel = "penjualan";
         $digit = "3";
+        $ymd = date('ymd');
 
-        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel LIMIT 1");
+        $q = $this->db->query("SELECT MAX(RIGHT($field,$digit)) AS kd_max FROM $tabel WHERE SUBSTR($field, 2, 6) = $ymd LIMIT 1");
         $kd = "";
         if ($q->num_rows() > 0) {
             foreach ($q->result() as $k) {
@@ -47,19 +49,19 @@ class M_home extends CI_Model
             $kd = "001";
         }
         date_default_timezone_set('Asia/Jakarta');
-        return 'P' . date('dmy') . $kd;
-	}
-	public function search($keyword)
-	{
-        $id_toko = $this->session->userdata('id_toko');
-        $this->db->like('kode_unik',$keyword);
-        $this->db->or_like('nama',$keyword);
-		return $this->db->get_where('barang_kasir', array('id_toko' => $id_toko, 'qty >' => 0))->result();
+        return 'P' . date('ymd') . $kd;
     }
-    
+    public function search($keyword)
+    {
+        $id_toko = $this->session->userdata('id_toko');
+        $this->db->like('kode_unik', $keyword);
+        $this->db->or_like('nama', $keyword);
+        return $this->db->get_where('barang_kasir', array('id_toko' => $id_toko, 'qty >' => 0))->result();
+    }
+
     function barang_qty_db($id_stok_b)
     {
-    $id_toko = $this->session->userdata('id_toko');
-    return $this->db->get_where('barang_kasir', array('id_toko' => $id_toko, 'qty >' => 0,'id_stok_b'=>$id_stok_b))->row();
+        $id_toko = $this->session->userdata('id_toko');
+        return $this->db->get_where('barang_kasir', array('id_toko' => $id_toko, 'qty >' => 0, 'id_stok_b' => $id_stok_b))->row();
     }
 }
