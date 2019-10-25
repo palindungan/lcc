5af0c54a0b5817f2554809566128a96e2690fd71
rizkikaZakka <rizkika.palindungan1@gmail.com>
2019-10-26 04:41:59 +0700

    ubah account
    ubah account

--- application/controllers/pimpinan/Edit_account.php
+++ application/controllers/pimpinan/Edit_account.php
@@ -18,10 +18,20 @@ class Edit_account extends CI_Controller
     }
     function index()
     {
-        $row = $this->M_edit_account->ambil_biodata();
-        $data['id'] = $row->id_user_p;
-        $data['nama'] = $row->nama;
-        $data['username'] = $row->username;
+
+        $id_user_p = $this->session->userdata('id_user'); // session
+
+        $data_id = array(
+            'id_user_p' => $id_user_p
+        );
+
+        $query = $this->M_edit_account->get_data('pimpinan', $data_id);
+        foreach ($query->result_array() as $row) {
+            $data['id'] = $row["id_user_p"];
+            $data['nama'] = $row["nama"];
+            $data['username'] = $row["username"];
+        }
+
         $this->template->load('view_1/template/pimpinan', 'view_1/konten/pimpinan/edit_account/tampil', $data);
     }
     function ubah_biodata()
@@ -44,16 +54,23 @@ class Edit_account extends CI_Controller
     }
     function ubah_password()
     {
-        $row = $this->M_edit_account->ambil_biodata();
-        $id = $row->id_user_p;
-        $password_lama = $row->password;
+        $id_user_p = $this->session->userdata('id_user'); // session
+        $data_id = array(
+            'id_user_p' => $id_user_p
+        );
+
+        $query = $this->M_edit_account->get_data('pimpinan', $data_id);
+        foreach ($query->result_array() as $row) {
+            $password_lama = $row["password"];
+        }
+
         $confirm_lama = $this->input->post('confirm_lama');
         $password_baru = $this->input->post('password_baru');
         $confirm_password = $this->input->post('confirm_password');
 
         if (password_verify($confirm_lama, $password_lama)) {
             if ($password_baru == $confirm_password) {
-                $where = array('id_user_p' => $id);
+                $where = array('id_user_p' => $id_user_p);
                 $data = array(
                     'password' => password_hash($password_baru, PASSWORD_DEFAULT)
                 );
